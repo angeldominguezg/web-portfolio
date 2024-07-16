8ae54d6c249c6edc0d6ab370e08fa6224e815ea0
Angel Dominguez <angeldominguezg@gmail.com>
2024-07-16 09:21:54 -0400

    Added Recent projects section

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,18 @@
 import Grid from "@/components/Grid";
+import RecentProjects from "@/components/RecentProjects";
 import { Hero } from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import { FaHome } from "react-icons/fa";
+import { navItems } from "@/data";
 
 export default function Home() {
-  const navItems = [
-    {
-      name: "Home",
-      link: "/",
-      icon: <FaHome className="h-4 w-4 text-neutral-500 dark:text-white" />,
-    },
-    // {
-    //   name: "About",
-    //   link: "/about",
-    //   // icon: <IconUser className="h-4 w-4 text-neutral-500 dark:text-white" />,
-    // },
-    // {
-    //   name: "Contact",
-    //   link: "/contact",
-    //   // icon: (
-    //   // <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />
-    //   // ),
-    // },
-  ];
-
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
+        <RecentProjects />
       </div>
     </main>
   );
